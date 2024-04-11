b8c5df98f774db2ea8517a00e73911a3d0ed0211
Raye <126174767+Dempserayo@users.noreply.github.com>
2024-04-11 18:44:14 -0500

    Ref<Footer>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { View, Text, SafeAreaView, ScrollView, TextInput } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
 
 const Footer = () => {
   return (
     <View className="flex justify-center">
-      <SafeAreaView style={{ flex: 1 }}></SafeAreaView>
+      <Text className="text-left font-bold  text-white mx-8 my-2">
+        <FontAwesome name="user-plus" size={32} color="white" />
+        <Text>POKEDEX</Text>
+      </Text>
     </View>
   );
 };
