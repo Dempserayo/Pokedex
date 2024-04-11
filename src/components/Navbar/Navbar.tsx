7087838abe74a0469fb9fc439bbd28a4eeff8de7
Raye <126174767+Dempserayo@users.noreply.github.com>
2024-04-11 18:13:37 -0500

    Ref<Navbar>

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { SafeAreaView, View, Text, TextInput } from "react-native";
-import { PokemonClient } from "pokenode-ts";
-import { FontAwesome } from "@expo/vector-icons";
+import React from "react";
+import { SafeAreaView, View, Text, TextInput, Button } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Navbar = () => {
   return (
-    <SafeAreaView className="bg-red-500 h-28 w-full">
-      <Text className="text-white font-bold text-2xl w-full text-center">
-        {" "}
-        <MaterialCommunityIcons
-          name="pokeball"
-          size={32}
-          color="white"
-          className="mx-20"
-        />{" "}
-        Pokedex{" "}
-      </Text>
+    <SafeAreaView className="bg-red-600  pt-6 w-full">
+      <View className="flex flex-row m-4">
+        <View className="rounded-full w-12 h-12 mx-2 border-4 border-gray-400 bg-blue-500"></View>
+        <View className="rounded-full w-4 h-4 mx-1 mt-4 border-4 border-red-600 bg-red-500"></View>
+        <View className="rounded-full w-4 h-4 mx-1 mt-4 border-4 border-yellow-600 bg-yellow-500"></View>
+        <View className="rounded-full w-4 h-4 mx-1 mt-4 border-4 border-green-600 bg-green-500"></View>
+      </View>
 
       <View className="flex flex-row justify-start items-center mt-4 mx-4">
         <TextInput
@@ -24,8 +18,13 @@ const Navbar = () => {
           placeholder="Pokedex"
         />
 
-        <Text className="bg-white rounded-full px-4 py-2 w-auto mb-4 mr-2">
-          #
+        <Text className="bg-white rounded-full px-4 py-2 mb-4 mr-2">
+          {" "}
+          <MaterialCommunityIcons
+            name="pokemon-go"
+            size={24}
+            color="black"
+          />{" "}
         </Text>
       </View>
     </SafeAreaView>
